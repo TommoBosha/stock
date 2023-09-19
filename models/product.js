8a6b0379ae7b7edc2039ee5f4dfb57eafcc9818f
TommoBosha <116563699+TommoBosha@users.noreply.github.com>
2023-09-19 14:43:58 +0300

    fix model product

--- models/product.js
+++ models/product.js
@@ -3,7 +3,7 @@ const { Schema, model, models, default: mongoose } = require("mongoose");
 const ProductSchema = new Schema({
     title: { type: String, required: true },
     description: String,
-    productIndex: { type: Number, required: true },
+    productIndex: { type: Number, required: true, unique: true, index: true },
     price: { type: Number, required: true },
     images: [{ type: String }],
     category: { type: mongoose.Types.ObjectId, ref: 'Category' },
