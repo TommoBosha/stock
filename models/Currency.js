d8355fc2b3d7403e4fb88100a9207f31f972b0b2
TommoBosha <116563699+TommoBosha@users.noreply.github.com>
2023-09-29 16:53:59 +0300

    add currency to dashboard

---
+++ models/Currency.js
@@ -0,0 +1,13 @@
+import mongoose, { model, models, Schema } from "mongoose";
+
+const CurrencySchema = new Schema({
+    currency: { type: String, required: true },
+    date: { type: Date, required: true },
+
+},
+    {
+        timestamps: true,
+    }
+);
+
+export const Currency = models?.Currency || model('Currency', CurrencySchema);
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,40 @@
+import Currency from "@/components/Currency";
 import Layout from "@/components/Layout";
+import axios from "axios";
 import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 
 
 export default function Home() {
   const { data: session } = useSession();
+  const [currency, setCurrency] = useState(null);
+
+  useEffect(() => {
+    fetchCurrency();
+  }, []);
+
+  function fetchCurrency() {
+    axios.get("/api/currency").then((result) => {
+      console.log(result.data)
+      setCurrency(result.data);
+    });
+  }
 
   return (
     <Layout>
-      <div className="text-blue-900 flex justify-between">
+      <div className="text-blue-900 flex justify-between items-center">
         <h2>
           Hello, <b>{session?.user?.name}</b>
         </h2>
-        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+        <div>
+          {currency ? (
+            <p>Останній курс валюти: {currency[currency.length - 1].currency} грн.</p>
+          ) : (
+            <p>Завантаження останнього курсу валюти...</p>
+          )}
+        </div>
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden ">
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             src={session?.user?.image}
@@ -24,6 +46,11 @@ export default function Home() {
           </span>
 
         </div>
+
+
+      </div>
+      <div className="mt-4">
+        <Currency fetchCurrency={fetchCurrency} />
 
       </div>
     </Layout>
